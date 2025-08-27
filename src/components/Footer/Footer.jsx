e80053561a89bdf3393d1dbe477c5be96fec3b9e
Özer Öztürk <60944453+OzerOzturk@users.noreply.github.com>
2025-08-27 11:50:02 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,12 +24,12 @@ const Footer = () => {
           <div className="footer__top__right__upper">
             <img src={qrCode} alt="qrCode__image" />
             <div className="footer__top__right__upper__text">
-              <h4>Çiçek Sepeti Mobil Uygulamayı İndirin</h4>
+              <h4>Ecommerce Mobil Uygulamayı İndirin</h4>
               <p>Mobil Uygulamayı QR Kod ile İndirin</p>
             </div>
           </div>
           <div className="footer__top__right__lower">
-            <a href="https://play.google.com/store/apps/details?id=com.ciceksepeti.ciceksepeti">
+            <a href="https://play.google.com/store/apps/details?id=com.ecommerce.react.app">
               <img src={playStore} alt="playStore__icon" />
             </a>
             <a href="https://apps.apple.com/app/id723715907">
@@ -40,26 +40,26 @@ const Footer = () => {
       </section>
       <section className="footer__bottom">
         <div className="footer__bottom__left">
-          <img src={logo} alt="ciceksepeti__logo" />
+          <img src={logo} alt="ecommerce__logo" />
           <div className="footer__bottom__left__social">
-            <a href="https://www.facebook.com/ciceksepeti">
+            <a href="https://www.facebook.com/ecommerce-react-app">
               <img src={facebook} alt="facebook__icon" />
             </a>
-            <a href="https://twitter.com/ciceksepeti">
+            <a href="https://twitter.com/ecommerce-react-app">
               <img src={twitter} alt="twitter__icon" />
             </a>
-            <a href="https://www.instagram.com/ciceksepeti/">
+            <a href="https://www.instagram.com/ecommerce-react-app/">
               <img src={instagram} alt="instagram__icon" />
             </a>
-            <a href="https://www.youtube.com/user/CicekSepeti">
+            <a href="https://www.youtube.com/user/ecommerce-react-app">
               <img src={youtube} alt="youtube__icon" />
             </a>
-            <a href="https://blog.ciceksepeti.com/">
-              <img src={blog} alt="ciceksepeti__blog__icon" />
+            <a href="https://blog.ecommerce-react-app/">
+              <img src={blog} alt="ecommerce__blog__icon" />
             </a>
           </div>
           <p className="footer__bottom__left__text">
-            CicekSepeti.com olarak kişisel verilerinizin gizliliğini
+            EcommerceReactApp olarak kişisel verilerinizin gizliliğini
             önemsiyoruz. 6698 sayılı Kişisel Verilerin Korunması Kanunu
             kapsamında oluşturduğumuz aydınlatma metnine&nbsp;
             <span>buradan</span>&nbsp;ulaşabilirsiniz.
@@ -70,11 +70,11 @@ const Footer = () => {
           <div className="footer__bottom__right__subheading">
             <h4>Faydalı Bilgiler</h4>
             <ul>
-              <li>Çiçek Bakımı</li>
-              <li>Çiçek Eşliğinde Notlar</li>
-              <li>Çiçek Anlamları</li>
+              <li>Plant Care</li>
+              <li>Plant Notes</li>
+              <li>Plant Meaning</li>
               <li>Özel Günler</li>
-              <li>Mevsimlere Göre Çiçekler</li>
+              <li>Plants for Seasons</li>
               <li>BonnyFood Saklama Koşulları</li>
               <li>Site Haritası</li>
             </ul>
@@ -84,7 +84,7 @@ const Footer = () => {
             <ul>
               <li>Hakkımızda</li>
               <li>Kariyer</li>
-              <li>ÇiçekSepeti’nde Satış Yap</li>
+              <li>EcommerceReact’de Satış Yap</li>
               <li>Kurumsal Müşterilerimiz</li>
               <li>Reklamlarımız</li>
               <li>Basında Biz</li>
@@ -112,21 +112,10 @@ const Footer = () => {
         </div>
       </section>
       <summary>
-        Türkiyenin en büyük online çiçek sitesi ÇiçekSepeti ile sevdiklerinizi
-        mutlu etmek çok kolay! Çiçek göndermenin ve “Mutlu etmenin adresi” Çiçek
-        Sepeti ile sevdiklerinize özel günlerde online çiçek gönderebilirsiniz.
-        Geniş çiçekçi ağı sayesinde çiçek siparişleriniz Türkiye’nin dört bir
-        yanına hızlı ve sorunsuz şekilde gönderilir. Taze çiçeklerle hazırlanan
-        mis kokulu şık çiçek aranjmanları arasından beğendiğiniz ürünü seçerek,
-        hızlı bir şekilde online sipariş verebilirsiniz. Sevdiklerinizin doğum
-        günü, yıldönümü gibi mutlu günlerinde onların sevincine ortak olmak için
-        yapmanız gereken sadece birkaç tıkla sipariş vermek. Sevgililer Günü,
-        Kadınlar Günü, Anneler Günü gibi özel günlerde de çiçek, hediye ve
-        lezzetli bonnyFood ürünleriyle sevdiklerinizi mutlu edebilirsiniz. Çünkü
-        mutlu etmenin adresi; ÇiçekSepeti.
+        Ecommerce React App, learning and practice project.
       </summary>
       <p className="footer__copyright">
-        Copyright © 2022 Çiçek Sepeti İnternet Hizmetleri A.Ş.
+        Copyright © 2287 EcommerceReactApp Inc.
       </p>
     </footer>
   );
