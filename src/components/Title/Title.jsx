0c583e00144e6222e554a1fb45b82cce330113d1
Özer Öztürk <60944453+OzerOzturk@users.noreply.github.com>
2025-08-27 11:54:19 +0300

    Update Title.jsx

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1 className="title__text">ÇiçekSepeti</h1>
+      <h1 className="title__text">EcommerceReactApp</h1>
     </div>
   );
 };
